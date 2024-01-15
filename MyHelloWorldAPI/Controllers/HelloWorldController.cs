e66f5da963e381572a5d090ef01e87c700bf58e2
unknown <s0920952886@gmail.com>
2024-01-15 11:04:25 +0800

    HelloWorld

---
+++ MyHelloWorldAPI/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHelloWorldAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        #region GetAPI
+
+        // 回傳Hello World
+        [HttpGet(Name = "GetHelloWorld")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+
+        #endregion
+    }
+}
